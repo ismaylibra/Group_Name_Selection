7f39618368b3abf448085d417c9dea270f559912
abbasoov <ismayiliib@code.edu.az>
2022-07-04 16:55:50 +0400

    modified

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -5,13 +5,14 @@ function GroupSelect(){
     
 
     if (groupNumber==1) {
-        document.write(`Your Group Number is ${groupName.toUpperCase()}`)
+        document.write(`Your Group Number is ${groupName.toUpperCase()} ${'<br>'} Morning Group`)
         document.body.style.backgroundColor = 'yellow'
         document.body.style.display = 'flex'
         document.body.style.alignItems='center'
         document.body.style.justifyContent= 'center'
         document.body.style.color = 'blue'
         document.body.style.fontSize = '50px'
+        document.body.style.textAlign = 'center'
         
         
         
@@ -19,24 +20,25 @@ function GroupSelect(){
         
     }
     else if (groupNumber==2) {
-        document.write(`Your Group Number is ${groupName.toUpperCase()}`)
+        document.write(`Your Group Number is ${groupName.toUpperCase()} ${'<br>'} Afternoon Group`)
         document.body.style.backgroundColor = 'red'
         document.body.style.display = 'flex'
         document.body.style.alignItems='center'
         document.body.style.justifyContent= 'center'
         document.body.style.color = 'white'
         document.body.style.fontSize = '50px'
-        groupTime = ''
+        document.body.style.textAlign = 'center'
         
     }
     else if (groupNumber==3) {
-        document.write(`Your Group Number is ${groupName.toUpperCase()}`)
+        document.write(`Your Group Number is ${groupName.toUpperCase()} ${'<br>'} Evening Group`)
         document.body.style.backgroundColor = 'black'
         document.body.style.display = 'flex'
         document.body.style.alignItems='center'
         document.body.style.justifyContent= 'center'
         document.body.style.color = 'white'
         document.body.style.fontSize = '50px'
+        document.body.style.textAlign = 'center'
         
     }
 }
